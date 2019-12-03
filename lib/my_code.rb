4b3b99cc47c7d0eef632c39fc2e1b7b280f64540
T Benson <t.benson@live.co.uk>
2019-12-03 00:29:19 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -42,3 +42,14 @@ def reduce_to_total(source_array, starting_point = 0)
   end
   total
 end
+
+def reduce_to_all_true(source_array)
+  i = 0
+  while i < source_array.length do
+    if source_array[i] != true
+      return false
+    end
+    i += 1
+  end
+  return true
+end
