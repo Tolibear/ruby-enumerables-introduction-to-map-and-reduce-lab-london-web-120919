ea1d2afb1c1fe9e9322b92b01d7367b387f1b985
T Benson <t.benson@live.co.uk>
2019-12-03 00:20:33 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,10 +10,15 @@ end
 
 def map_to_no_change(source_array)
   new_array = source_array
-  # i = 0
-  # while i < source_array.length do
-  #   new_array << ( source_array[i] * -1 )
-  #   i += 1
-  # end
+  new_array
+end
+
+def map_to_double(source_array)
+  new_array = []
+  i = 0
+  while i < source_array.length do
+    new_array << ( source_array[i] * 2 )
+    i += 1
+  end
   new_array
 end
