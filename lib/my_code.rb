2571000d0df552ac4cca62c67e7c2a8f5feceddb
T Benson <t.benson@live.co.uk>
2019-12-03 00:23:48 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -32,3 +32,13 @@ def map_to_square(source_array)
   end
   new_array
 end
+
+def reduce_to_total(source_array, starting_point = 0)
+  total = 0
+  i = starting_point
+  while i < source_array.length do
+    total += source_array[i] 
+    i += 1
+  end
+  total
+end
